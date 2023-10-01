192b1a32c13be1e2734a5902b7eda80cca62756a
tanngo1605 <50272836+tanngo1605@users.noreply.github.com>
2023-09-30 20:24:27 -0700

    test get more data

--- testGetData.ts
+++ testGetData.ts
@@ -1,24 +1,33 @@
-import prisma from "./prisma/prismaDB"
+import prisma from "./prisma/prismaDB";
 
 async function main() {
   const usersWithPosts = await prisma.user.findMany({
-    include:{
+    include: {
       Post: true,
-      Reaction: true
+      Reaction: true,
     },
-    orderBy:{
-      username: "asc"
-    }
-  })
-  console.log(usersWithPosts)
+    orderBy: {
+      username: "asc",
+    },
+  });
+  console.log({ usersWithPosts });
+
+  const posts = await prisma.post.findMany({
+    include: {
+      author: true,
+      Reaction: true,
+    },
+  });
+
+  console.log({ posts });
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
